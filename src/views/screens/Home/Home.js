8709c7ee0d45aebab16d3fd3870c1fec8e484e71
shiva.gunasehkaran <shivagunasehkaran@rea-group.com>
2019-12-10 18:21:43 +0800

    Added Static FlatList to show lift logic

--- src/views/screens/Home/Home.js
+++ src/views/screens/Home/Home.js
@@ -1,32 +1,68 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome5';
-
+import {Button, Text, View, StyleSheet, FlatList, Alert} from 'react-native';
 import theme from '../../../themes';
 
 class Home extends Component {
     static navigationOptions = ({navigation}) => ({
-        title: ('Demo Lift'),
+        title: ('Home Screen'),
         headerStyle: {
             backgroundColor: theme.colors.buttonColor,
         },
         headerRight: (
-            <Icon style={{paddingRight: 15, color: theme.colors.black}}
-                  onPress={() => navigation.navigate('Detail')}
-                  name="plus"
-                  size={20} />
+            <Button style={{paddingRight: 50, color: theme.colors.black}}
+                    onPress={() => navigation.navigate('Detail')}
+                    title={('Start Run')} />
         ),
     });
 
+    renderSeparator = () => {
+        return (
+            <View
+                style={{
+                    height: 1,
+                    width: '100%',
+                    backgroundColor: '#000',
+                }}
+            />
+        );
+    };
+    //handling onPress action
+    getListViewItem = (item) => {
+        Alert.alert(item.key);
+    };
+
     render() {
         return (
-            <View>
-                <Text>
-                    {('Home')}
-                </Text>
+            <View style={styles.container}>
+                <FlatList
+                    data={[
+                        {key: 'Lift Floor 6'},
+                        {key: 'Lift Floor 5'},
+                        {key: 'Lift Floor 4'},
+                        {key: 'Lift Floor 3'},
+                        {key: 'Lift Floor 2'},
+                        {key: 'Lift Floor 1'},
+                        {key: 'Lift Floor G'},
+                    ]}
+                    renderItem={({item}) =>
+                        <Text style={styles.item}
+                              onPress={this.getListViewItem.bind(this, item)}>{item.key}</Text>}
+                    ItemSeparatorComponent={this.renderSeparator}
+                />
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 100,
+    },
+});
 
 export default Home;
